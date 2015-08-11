3c221e9d4db8aeb33839083218a497d6adc6789e
Max Goedjen <max.goedjen@gmail.com>
2015-08-11 01:58:03 -0700

    Fill out initializers for media item

--- M3U8/MediaItem.swift
+++ M3U8/MediaItem.swift
@@ -12,13 +12,44 @@ public struct MediaItem: Item {
 
     public let type: String
     public let groupID: String
-    public let language: String
-    public let associatedLanguage: String
-    public let autoSelect: Bool
-    public let isDefault: Bool
-    public let uri: String
-    public let forced: Bool
+    public let name: String
+    public let language: String?
+    public let associatedLanguage: String?
+    public let autoSelect: Bool?
+    public let isDefault: Bool?
+    public let forced: Bool?
+    public let uri: String?
     
+    public init(type: String, groupID: String, name: String, language: String? = nil, associatedLanguage: String? = nil, autoSelect: Bool? = nil, isDefault:Bool? = nil, forced: Bool? = nil, uri: String? = nil) {
+        self.type = type
+        self.groupID = groupID
+        self.name = name
+        self.language = language
+        self.associatedLanguage = associatedLanguage
+        self.autoSelect = autoSelect
+        self.isDefault = isDefault
+        self.forced = forced
+        self.uri = uri
+    }
+    
+    public init?(string: String) {
+        let attributes = string.m3u8Attributes
+        guard let type = attributes["TYPE"],
+        let groupID = attributes["GROUP-ID"],
+            let name = attributes["NAME"]
+        else {
+            return nil
+        }
+        self.type = type
+        self.groupID = groupID
+        self.name = name
+        language = attributes["LANGUAGE"]
+        associatedLanguage = attributes["ASSOC-LANGUAGE"]
+        autoSelect = Bool(string: attributes["AUTOSELECT"])
+        isDefault = Bool(string: attributes["DEFAULT"])
+        forced = Bool(string: attributes["FORCED"])
+        uri = attributes["URI"]
+    }
 }
 
 extension MediaItem {
