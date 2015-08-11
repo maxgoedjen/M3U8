9e664ea957c7b445ed74ed321a59f67ebdcf9a13
Max Goedjen <max.goedjen@gmail.com>
2015-08-11 00:30:28 -0700

    Add initializers to keyitem

--- M3U8/KeyItem.swift
+++ M3U8/KeyItem.swift
@@ -10,11 +10,28 @@ import Foundation
 
 public struct KeyItem {
     
-    public let method: String
-    public let uri: String
-    public let iv: String
-    public let keyFormat: String
-    public let keyFormatVersions: [String]
+    public let method: String?
+    public let uri: String?
+    public let iv: String?
+    public let keyFormat: String?
+    public let keyFormatVersions: String?
+    
+    public init(method: String? = nil, uri: String? = nil, iv: String? = nil, keyFormat: String? = nil, keyFormatVersions: String? = nil) {
+        self.method = method
+        self.uri = uri
+        self.iv = iv
+        self.keyFormat = keyFormat
+        self.keyFormatVersions = keyFormatVersions
+    }
+    
+    public init(string: String) {
+        let attributes = string.attributes
+        method = attributes["METHOD"]
+        uri = attributes["URI"]
+        iv = attributes["IV"]
+        keyFormat = attributes["KEYFORMAT"]
+        keyFormatVersions = attributes["KEYFORMATVERSIONS"]
+    }
     
 }
 
