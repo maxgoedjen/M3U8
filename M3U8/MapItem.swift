d468d6fb4f3103bcc938ec16f375265979e947a0
Max Goedjen <max.goedjen@gmail.com>
2015-08-11 01:26:16 -0700

    Filling out map item

--- M3U8/MapItem.swift
+++ M3U8/MapItem.swift
@@ -10,12 +10,37 @@ import Foundation
 
 public struct MapItem: Item {
     
+    public let uri: String
+    public let byteRange: ByteRange?
+    
+    public init(uri: String, byteRange: ByteRange? = nil) {
+        self.uri = uri
+        self.byteRange = byteRange
+    }
+    
+    public init?(string: String) {
+        let attributes = string.m3u8Attributes
+        guard let uri = attributes["URI"] else {
+            return nil
+        }
+        self.uri = uri
+        if let byteRangeString = attributes["BYTERANGE"] {
+            byteRange = ByteRange(string: byteRangeString)
+        } else {
+            byteRange = nil
+        }
+    }
+    
 }
 
 extension MapItem {
     
     public var description: String {
-        return ""
+        var strung = "#EXT-X-MAP:URI=\"\(uri)\""
+        if let byteRangeString = byteRange?.description {
+            strung += ",BYTERANGE=\"\(byteRangeString)\""
+        }
+        return strung
     }
     
 }
